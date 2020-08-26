bc126881053c9e5cf8db4cdbada4c0f1b849dfec
torinosubako <59300206+torinosubako@users.noreply.github.com>
2020-08-26 21:52:12 +0900

    Ewigkeit(R20826v1)
    修正
    赤外線LED点灯時に同時に赤色LEDを点灯するように修正
    東芝のTVの電源制御に対応

--- louder/louder.ino
+++ louder/louder.ino
@@ -9,6 +9,7 @@
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 
+// M5StickCのIRは9ピン
 const uint16_t Ir_Led = 9;
 IRsend irsend(Ir_Led);
 
@@ -16,9 +17,11 @@ void setup() {
   irsend.begin();
 
   M5.begin();
+  pinMode(GPIO_NUM_10, OUTPUT);
   M5.Lcd.setRotation(1);
   M5.Lcd.fillScreen(TFT_BLACK);
-  M5.Lcd.println("IR Remote Control Test");
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.println("IR Remote Control");
   M5.Lcd.println("Press M5 button");
 }
 
@@ -28,7 +31,13 @@ void loop() {
   // M5ボタン(BtnA)が押されたとき
   if (M5.BtnA.wasPressed()) {
     // ここで赤外線信号を送信する
-    irsend.sendNEC(0x41C4F807, 32);
+    // 山善・扇風機・電源
+    //irsend.sendNEC(0x41C4F807, 32);
+    //delay(100);
+    //東芝・液晶テレビ・電源
+    digitalWrite(GPIO_NUM_10, LOW);
+    irsend.sendNEC(0x2FD48B7, 32);
     delay(100);
+    digitalWrite(GPIO_NUM_10, HIGH);
   }
 }
